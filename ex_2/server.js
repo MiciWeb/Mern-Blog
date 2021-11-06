862744ecf9ec8f627e7c99ba37b7d7003b9a2974
Mici Srsr <micipsa.sersour@epitech.eu>
2021-11-06 16:13:19 +0100

    add registering with react

--- ex_2/server.js
+++ ex_2/server.js
@@ -28,8 +28,8 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-
 app.use(cors())
+
 
 app.get('/billets', function (req, res) {
     db.collection('billets').find({}).toArray(function (err, data) {
@@ -37,77 +37,46 @@ app.get('/billets', function (req, res) {
         res.status(200).json(data);
     })
 })
-app.post('/register', function (req, res) {
-    console.log(req.body)
-    db.collection('billets').insertOne({
-        // _id: Math.floor(Math.random() * 1000000),
-        // login: req.body.login,
-        // email: req.body.email,
-        // password: sha1(req.body.password),
-        // type: false
-        title: req.body.title,
-        body: req.body.body,
-    }, function (err, inserted) {
-        if (err) {
-            res.status(400).json("There is an error in the fields !")
-            res.end()
+
+app.post("/register", (req, res) => {
+    // check if email or login already exist then insert to database
+    db.collection("users").find({ $and: [{ email: req.body.email }] }).toArray(function (err, data) {
+        var mail = [];
+        data.forEach(element => {
+            mail.push(element.email)
+        });
+        if (mail.length > 0) {
+            res.status(400).json("Email already taken !")
         } else {
-            res.status(200).json("Ok good");
-            res.end()
+            db.collection("users").find({ $and: [{ login: req.body.login }] }).toArray(function (err, data) {
+                var login = [];
+                data.forEach(element => {
+                    login.push(element.login)
+                });
+                if (login.length > 0) {
+                    res.status(400).json("Login already taken !")
+                } else {
+                    db.collection('users').insertOne({
+                        _id: Math.floor(Math.random() * 100000000),
+                        login: req.body.login,
+                        email: req.body.email,
+                        password: sha1(req.body.password),
+                        type: false
+                    }, function (err) {
+                        if (err) {
+                            res.status(400).json("There is an error in the fields !")
+                            res.end()
+                        } else {
+                            res.status(200).json("You can now login !");
+                            res.end()
+                        }
+                    });
+
+                }
+            })
         }
-    });
+    })
 })
-
-// app.get("/register", (req, res) => {
-//     res.render("register", { error: "" })
-// })
-
-// app.post("/register", (req, res) => {
-//     db.collection("users").find({ $and: [{ email: req.body.email }] }).toArray(function (err, data) {
-//         var mail = [];
-//         // var login = [];
-//         data.forEach(element => {
-//             mail.push(element.email)
-//             // login.push(element.login)
-//             // console.log(element)
-//         });
-//         // console.log(login.length)
-//         if (mail.length > 0) {
-//             res.status(400).render('register', { error: "Email already taken !" })
-//         } else {
-//             db.collection("users").find({ $and: [{ login: req.body.login }] }).toArray(function (err, data) {
-//                 var login = [];
-//                 data.forEach(element => {
-//                     login.push(element.login)
-//                 });
-//                 console.log(login.length)
-//                 if (login.length > 0) {
-//                     res.status(400).render('register', { error: "Login already taken !" })
-//                 } else {
-//                     if (req.body.password !== req.body.password2) {
-//                         res.status(400).render('register', { error: "Password dont match !" })
-//                     } else {
-//                         db.collection('users').insertOne({
-//                             _id: Math.floor(Math.random() * 1000000),
-//                             login: req.body.login,
-//                             email: req.body.email,
-//                             password: sha1(req.body.password),
-//                             type: false
-//                         }, function (err, inserted) {
-//                             if (err) {
-//                                 res.status(400).render('register', { error: 'There is an error in the fields !' })
-//                                 res.end()
-//                             } else {
-//                                 res.status(200).render("index", { error: req.body.login });
-//                             }
-//                         });
-//                     }
-//                 }
-//             })
-//         }
-//     })
-
-// })
 
 // app.get('/login', function (req, res) {
 //     res.render('login', { error: "" })
